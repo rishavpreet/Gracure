074c871b16b2d40a1f8bed2f2f1533befe4b7d0c
Harpreet Kaur <harpreesidhu1997@gmail.com>
2023-03-09 17:13:05 +0530

    update crud.py

--- Server/gracure_app/crud.py
+++ Server/gracure_app/crud.py
@@ -13,16 +13,16 @@ log.setLevel(logging.DEBUG)
 
 async def create_user_data(db: Session, data: schemas.UserDataCreate):
     try:
-        db_master = models.UserData(group_level=data.group_level, username=data.username,
+        db_data = models.UserData(group_level=data.group_level, username=data.username,
                                     password=hashlib.md5(data.password.encode('utf-8')).hexdigest(),
                                     create_date=data.create_date, user_status=data.user_status,
                                     password_attempt=data.password_attempt, department=data.department,
                                     new_user=data.new_user)
 
-        db.add(db_master)
+        db.add(db_data)
         db.commit()
-        db.refresh(db_master)
-        return db_master
+        db.refresh(db_data)
+        return db_data
     except Exception as e:
         print(e)
 
@@ -30,13 +30,13 @@ async def create_user_data(db: Session, data: schemas.UserDataCreate):
 # ..........................................................................................
 async def create_duplicate_password(db: Session, data: schemas.DuplicatePasswordCreate):
     try:
-        db_master = models.DuplicatePassword(username=data.username,
+        db_data = models.DuplicatePassword(username=data.username,
                                              password=hashlib.md5(data.password.encode('utf-8')).hexdigest(),
                                              create_date=data.create_date)
-        db.add(db_master)
+        db.add(db_data)
         db.commit()
-        db.refresh(db_master)
-        return db_master
+        db.refresh(db_data)
+        return db_data
     except Exception as e:
         print(e)
 
